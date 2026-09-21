b54147b518b7c0798f07eb1b35ccb89f01a461ad
Sarthak Negi <sarthakn658@gmail.com>
2026-09-20 22:29:43 -0700

    adding data for the shop section

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -1,11 +1,34 @@
-import React from 'react'
-import Herosection from '../components/Herosection'
+import ProductCard from "../components/productCard";
+import products from "../assets/products";
+import { useSearch } from "../context/searContext";
 const Shop = () => {
-  return (
-    <div>
-      <div>Shop</div>
-    </div>
-  )
-}
+  const { search } = useSearch();
 
+  const menProducts = products.filter((product) => {
+    const matchesSearch = product.name
+      .toLowerCase()
+      .includes(search.toLowerCase());
+
+    return (product.category === "Men" || product.category === "Women") && matchesSearch;
+  });
+
+  return (
+    <section className="p-4 sm:p-6 lg:p-8">
+
+      <h1 className="text-3xl font-bold mb-8">
+        All Products
+      </h1>
+
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+        {menProducts.map((product: (typeof products)[number]) => (
+          <ProductCard
+            key={product.id}
+            product={product}
+          />
+        ))}
+      </div>
+
+    </section>
+  );
+};
 export default Shop
